eef8d3dff81170936ed76dc8a7cba383f93b82f9
OOF-Crew <72276710+OOF-Crew@users.noreply.github.com>
2020-11-15 21:24:02 +0100

    Update config.py

--- skylee/config.py
+++ skylee/config.py
@@ -5,7 +5,7 @@ class Development(Config):
     OWNER_ID =  816157233 # my telegram ID
     OWNER_USERNAME = "doggy_cheems"  # my telegram username
     API_KEY = "1221978373:AAENSOKIr8FXnJ9huhzRrH8idnL4leBq7kc"  # my api key, as provided by the botfather
-    SQLALCHEMY_DATABASE_URI = 'postgres://weygupfewpcjls:eff163b09baec6bcac08a06268048ebe1b56064fe9e7bd18ceb502c8127bf37b@ec2-34-204-121-199.compute-1.amazonaws.com:5432/deupr96st7c30i'  # sample db credentials
+    SQLALCHEMY_DATABASE_URI = 'postgres://mcawowehvphzpg:5065c9e95fcaa4d5778f8d83ac331cecd7817b7022ae45f08d6ff8421431329f@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/d9kp79im13flj5'  # sample db credentials
     MESSAGE_DUMP = '-354482383' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
     SUDO_USERS = []  # List of id's for users which have sudo access to the bot.
