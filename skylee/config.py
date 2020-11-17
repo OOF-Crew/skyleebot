91700c8b0028d3a8c812c5ae2470a70aafa76a5c
OOF-Crew <72276710+OOF-Crew@users.noreply.github.com>
2020-11-17 21:41:59 +0100

    Update config.py

--- skylee/config.py
+++ skylee/config.py
@@ -8,9 +8,9 @@ class Development(Config):
     SQLALCHEMY_DATABASE_URI = 'postgres://mcawowehvphzpg:5065c9e95fcaa4d5778f8d83ac331cecd7817b7022ae45f08d6ff8421431329f@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/d9kp79im13flj5'  # sample db credentials
     MESSAGE_DUMP = '-354482383' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
-    SUDO_USERS = "1037394974" "1343416318"  # List of id's for users which have sudo access to the bot.
-    SUPPORT_USERS = "1037394974" "1343416318"
-    WHITELIST_USERS = "1037394974" "1343416318"
+    SUDO_USERS = "1037394974" "1343416318" "162479449" "618317358" # List of id's for users which have sudo access to the bot.
+    SUPPORT_USERS = "1037394974" "1343416318" "162479449" "618317358"
+    WHITELIST_USERS = "1037394974" "1343416318" "162479449" "618317358"
     LOAD = []
     NO_LOAD = []
     TELETHON_HASH = "ff3b1871585d32a794465da51b077361" # for purge stuffs
