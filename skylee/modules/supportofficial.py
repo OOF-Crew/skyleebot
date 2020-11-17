60081e08f547109c379a056637019053aec77009
OOF-Crew <72276710+OOF-Crew@users.noreply.github.com>
2020-11-17 22:08:49 +0100

    Update supportofficial.py

--- skylee/modules/supportofficial.py
+++ skylee/modules/supportofficial.py
@@ -18,15 +18,15 @@ from skylee.modules.helper_funcs.alternate import typing_action
 @run_async
 @typing_action
 def admin_list(update, context):
-    STAFF UFFICIALE DEL BOT
-    owner e gestore codice 👑: @doggycheems 
-    Gestori fedban⛔️: @doggycheems @marvynstar @TheDarknessCatGty @Camminatoredeighiacci @lEGIONARIO_ROMANO\n"
+    support = STAFF UFFICIALE DEL BOT
+    support = owner e gestore codice 👑: @doggycheems 
+    support = Gestori fedban⛔️: @doggycheems @marvynstar @TheDarknessCatGty @Camminatoredeighiacci @lEGIONARIO_ROMANO\n"
 
-    Supporter👍: @marvynstar @TheDarknessCatGty @Camminatoredeighiacci @lEGIONARIO_ROMANO\n"
-    ATTENZIONE\n"
-    Il bot è in fase di beta testing ed è disponibile solo per gruppi di @doggycheems o autorizzati\n"
-    Vuoi essere beta tester? scrivi a @doggycheems e chiedi ti darà tutti i dettagli necessari\n"
-    context.bot.sendMessage(update.effective_chat.id, status, parse_mode=ParseMode.HTML)
+    support = Supporter👍: @marvynstar @TheDarknessCatGty @Camminatoredeighiacci @lEGIONARIO_ROMANO\n"
+    support = ATTENZIONE\n"
+    support = Il bot è in fase di beta testing ed è disponibile solo per gruppi di @doggycheems o autorizzati\n"
+    support = Vuoi essere beta tester? scrivi a @doggycheems e chiedi ti darà tutti i dettagli necessari\n"
+    support = context.bot.sendMessage(update.effective_chat.id, support, parse_mode=ParseMode.HTML)
 
 ADMIN_LIST_HANDLER = CommandHandler(
     "staffbot", admin_list, filters=CustomFilters.sudo_filter
